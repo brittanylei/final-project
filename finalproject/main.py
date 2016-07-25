254d0038d1c80542ab6327ca1d328e69dc73dbf9
Demo User <joseph.dr.medina@gmail.com>
2016-07-25 15:43:09 -0700

    added sign in page

--- finalproject/main.py
+++ finalproject/main.py
@@ -17,11 +17,6 @@
 import webapp2
 import jinja2
 import os
-<<<<<<< HEAD
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
-=======
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
@@ -35,7 +30,7 @@ class Books(ndb.Model):
     author = ndb.StringProperty()
     date_published = ndb.DateTimeProperty()
     ISBN = ndb.IntegerProperty()
->>>>>>> f5d7ebc81686d71a7a591f913d836aa7e1b4d2e1
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -52,11 +47,13 @@ class BookHandler(webapp2.RequestHandler):
         template= jinja_environment.get_template('book.html')
         self.response.out.write(template.render())
 
-
-
-
+class SignInHandler(webapp2.RequestHandler):
+    def get(self):
+        template= jinja_environment.get_template('book.html')
+        self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/results', ResultsHandler)
+    ('/results', ResultsHandler),
+    ('/signin', )
 ], debug=True)
